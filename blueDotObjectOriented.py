7445a6261ab30aa15e55c8f14fe3f1ddb6741b11
Tom Neuhold-Huber <tneuhold@adobe.com>
2020-08-09 20:53:34 -0700

    first commit

---
+++ blueDotObjectOriented.py
@@ -0,0 +1,90 @@
+import arcade
+
+# Constants
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 800
+SCREEN_TITLE = "Welcome to Arcade OO"
+RADIUS = 150
+
+
+# Class
+class Welcome(arcade.Window):
+    # Main welcome window
+    def __init__(self):
+        # Initialize window
+
+        # Call the parent class constructor
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+ 
+        # Set the background color 
+        arcade.set_background_color(arcade.color.WHITE)
+
+    def on_draw(self):
+        #Called whenever you need to draw our window
+
+        # Clear the screen and start drawing
+        arcade.start_render()
+
+        # Draw a blue circle
+        arcade.draw_circle_filled(
+            SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, RADIUS, arcade.color.BLUE
+        )
+
+    def on_key_press(self, symbol, modifiers):
+        """Handle user keyboard input
+        Q: Quit the game
+        P: Pause/Unpause the game
+        I/J/K/L: Move Up, Left, Down, Right
+        Arrows: Move Up, Left, Down, Right
+
+        Arguments:
+            symbol {int} -- Which key was pressed
+            modifiers {int} -- Which modifiers were pressed
+        """
+        if symbol == arcade.key.Q:
+            # Quit immediately
+            arcade.close_window()
+
+        if symbol == arcade.key.P:
+            self.paused = not self.paused
+
+        if symbol == arcade.key.I or symbol == arcade.key.UP:
+            self.player.change_y = 5
+
+        if symbol == arcade.key.K or symbol == arcade.key.DOWN:
+            self.player.change_y = -5
+
+        if symbol == arcade.key.J or symbol == arcade.key.LEFT:
+            self.player.change_x = -5
+
+        if symbol == arcade.key.L or symbol == arcade.key.RIGHT:
+            self.player.change_x = 5
+
+    def on_key_release(self, symbol: int, modifiers: int):
+        """Undo movement vectors when movement keys are released
+
+        Arguments:
+            symbol {int} -- Which key was pressed
+            modifiers {int} -- Which modifiers were pressed
+        """
+        if (
+            symbol == arcade.key.I
+            or symbol == arcade.key.K
+            or symbol == arcade.key.UP
+            or symbol == arcade.key.DOWN
+        ):
+            self.player.change_y = 0
+
+        if (
+            symbol == arcade.key.J
+            or symbol == arcade.key.L
+            or symbol == arcade.key.LEFT
+            or symbol == arcade.key.RIGHT
+        ):
+            self.player.change_x = 0
+
+# Main code entry point
+if __name__ == "__main__":
+    app = Welcome()
+    # Display everything
+    arcade.run()
