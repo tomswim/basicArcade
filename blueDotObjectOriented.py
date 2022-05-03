4995d88a0d08bc23bf1de30ef0a6aed19bd21b3f
Tom Neuhold-Huber <tomswim@mac.com>
2022-05-02 20:15:44 -0700

    update

--- blueDotObjectOriented.py
+++ blueDotObjectOriented.py
@@ -30,58 +30,7 @@ class Welcome(arcade.Window):
             SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, RADIUS, arcade.color.BLUE
         )
 
-    def on_key_press(self, symbol, modifiers):
-        """Handle user keyboard input
-        Q: Quit the game
-        P: Pause/Unpause the game
-        I/J/K/L: Move Up, Left, Down, Right
-        Arrows: Move Up, Left, Down, Right
 
-        Arguments:
-            symbol {int} -- Which key was pressed
-            modifiers {int} -- Which modifiers were pressed
-        """
-        if symbol == arcade.key.Q:
-            # Quit immediately
-            arcade.close_window()
-
-        if symbol == arcade.key.P:
-            self.paused = not self.paused
-
-        if symbol == arcade.key.I or symbol == arcade.key.UP:
-            self.player.change_y = 5
-
-        if symbol == arcade.key.K or symbol == arcade.key.DOWN:
-            self.player.change_y = -5
-
-        if symbol == arcade.key.J or symbol == arcade.key.LEFT:
-            self.player.change_x = -5
-
-        if symbol == arcade.key.L or symbol == arcade.key.RIGHT:
-            self.player.change_x = 5
-
-    def on_key_release(self, symbol: int, modifiers: int):
-        """Undo movement vectors when movement keys are released
-
-        Arguments:
-            symbol {int} -- Which key was pressed
-            modifiers {int} -- Which modifiers were pressed
-        """
-        if (
-            symbol == arcade.key.I
-            or symbol == arcade.key.K
-            or symbol == arcade.key.UP
-            or symbol == arcade.key.DOWN
-        ):
-            self.player.change_y = 0
-
-        if (
-            symbol == arcade.key.J
-            or symbol == arcade.key.L
-            or symbol == arcade.key.LEFT
-            or symbol == arcade.key.RIGHT
-        ):
-            self.player.change_x = 0
 
 # Main code entry point
 if __name__ == "__main__":
